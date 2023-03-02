960246a323ddc06ba702201dac30b41bc38dc0a4
Elliot Sayes <elliot@sayes.cloud>
2023-03-02 22:29:13 +0800

    Remove dio base options... These seem to cancel the download after 16s

--- lib/src/ardrive_http.dart
+++ lib/src/ardrive_http.dart
@@ -39,10 +39,10 @@ class ArDriveHTTP {
 
   Dio _dio() {
     final dio = Dio(
-      BaseOptions(
-        connectTimeout: const Duration(seconds: 8),
-        receiveTimeout: const Duration(seconds: 8),
-      ),
+      // BaseOptions(
+      //   connectTimeout: const Duration(seconds: 8),
+      //   receiveTimeout: const Duration(seconds: 8),
+      // ),
     );
 
     if (!noLogs) {
